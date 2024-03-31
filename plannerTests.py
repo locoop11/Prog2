071582153c43e0e61d9ecb0c4c0d5c8c901b174b
PiriDrak <higino@gmail.com>
2024-03-31 19:25:59 +0100

    First tests to planner

--- plannerTests.py
+++ plannerTests.py
@@ -1,3 +1,5 @@
+import Doctor
+from planner import *
 
 class TestPlanner :
     def __init__(self) :
@@ -5,6 +7,16 @@ class TestPlanner :
 
     def runTests (self) :
         pass
+
+    def testPrioritizeDoctors() :
+        doctorsString = "Abílio Amaral, 1, 11h05, 180, 28h00\nBernardo Biscaia, 3, 10h50, 100, 7h40\nCarlos Castro, 2, 11h55, 240, 32h20\nDuarte Dantas, 3, 11h00, 290, 15h20"
+        doctorList = doctorsString.split("\n")
+        objectList = []
+        for line in doctorList :
+            (nome, experiencia, ultimoParto, minAcomulados, ultimoDescanso) = line.split(", ")
+            objectList.append(Doctor(nome, experiencia, ultimoParto, minAcomulados, ultimoDescanso))
+        planner = planner(objectList, [], [])
+        planner.prioritizeDoctors()
 
     def testPlannerGetters() :
         # Create a planner and then
